b9d5feb674f1d987c27343d0bd3d56e671d88473
Ian Butler <itb206@nyu.edu>
2017-11-16 11:56:49 -0500

    Client supports our model signature.

--- predict_client/prod_client.py
+++ predict_client/prod_client.py
@@ -22,13 +22,10 @@ class PredictClient(AbstractPredictClient):
         logger.info('Model version: ' + str(self.model_version))
         logger.info('Host: ' + str(self.host))
 
-        tensor_shape = request_data.shape
-
-        if self.model_name == 'incv4' or self.model_name == 'res152':
-            features_tensor_proto = tf.contrib.util.make_tensor_proto(request_data, shape=tensor_shape)
-        else:
-            features_tensor_proto = tf.contrib.util.make_tensor_proto(request_data,
-                                                                      dtype=tf.float32, shape=tensor_shape)
+        input_tensor_shape = request_data['input'].shape
+        sz_tensor_shape = request_data['input_sz'].shape
+        input_tensor_proto = tf.contrib.util.make_tensor_proto(request_data['input'], dtype=tf.int32, shape=input_tensor_shape)
+        sz_tensor_proto = tf.contrib.util.make_tensor_proto(request_data['input_sz'], shape=sz_tensor_shape, dtype=tf.int32)
 
         # Create gRPC client and request
         channel = grpc.insecure_channel(self.host)
@@ -40,7 +37,8 @@ class PredictClient(AbstractPredictClient):
         if self.model_version > 0:
             request.model_spec.version.value = self.model_version
 
-        request.inputs['inputs'].CopyFrom(features_tensor_proto)
+        request.inputs['input'].CopyFrom(input_tensor_proto)
+        request.inputs['input_sz'].CopyFrom(sz_tensor_proto)
 
         try:
             result = stub.Predict(request, timeout=request_timeout)
